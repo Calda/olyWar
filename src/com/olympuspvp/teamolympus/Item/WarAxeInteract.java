093f8229d6ab67048706fe97b15e0f4d42906da4
CalDaBeast <caldabeast@olympuspvp>
2012-06-15 20:30:20 -0400

    b0.1.2 - Attempting to fix critical crash error due to WarAxe loop

--- src/com/olympuspvp/teamolympus/Item/WarAxeInteract.java
+++ src/com/olympuspvp/teamolympus/Item/WarAxeInteract.java
@@ -1,24 +1,17 @@
 package com.olympuspvp.teamolympus.Item;
 
-import java.util.List;
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 
 public class WarAxeInteract {
 
-	public static void run(LivingEntity e, Player p){
-		e.damage(3, p);
-		List<Entity> nearby = e.getNearbyEntities(2, 2, 2);
-		for(Entity ent : nearby){
-			if(ent instanceof Player){
-				Player damage = (Player) ent;
-				if(damage.equals(p)){
-				}else{
-					damage.damage(3, p);
+	public static void run(LivingEntity e, LivingEntity damager){
+		for(Entity ent : e.getNearbyEntities(2,2,2)){
+			if(ent instanceof LivingEntity){
+				if(!ent.equals(damager)){
+					e.damage(3);
 				}
 			}
-		}
+		}e.damage(3);
 	}
 }
